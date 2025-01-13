834173b3a53974dbe09c7fa876812d52376bdda9
Kenner <kenner.jamieson@ucalgary.ca>
2025-01-13 13:57:59 -0700

    Updated portfolio

--- src/data/data.js
+++ src/data/data.js
@@ -4,8 +4,8 @@ const data = {
     name: "Kenner Jamieson",
     bio: "Undergraduate Architectural Student at University of Calgary passionate about industrial and graphic design",
     contact: {
-        email: "john.doe@gmail.com",
-        phone: "+12345678910",
+        email: "Kenner.Jamieson@ucalgary.ca",
+        phone: "780-434-5464",
         linkedin: "https://www.linkedin.com/",
         github: "https://www.github.com/",
     }

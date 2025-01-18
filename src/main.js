b44f82009b06c7eea0f229bf91af77b26b2a40d8
Kenner <kenner.jamieson@ucalgary.ca>
2025-01-17 19:04:16 -0700

    I made changes yes whahoo :)

--- src/main.js
+++ src/main.js
@@ -34,7 +34,7 @@ document.querySelector("#app").innerHTML = `
       <div id="project-row">
       Model 1
         <div class="three-model">
-          <div id="model1"></div>
+          <img src="./houseDrawings/house1.jpg" id="model1" alt="Model 1 Image" />
         </div>
         <div id="images-description">
           <div id="images">
